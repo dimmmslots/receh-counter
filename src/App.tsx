3ac02623939005e7e82c7d67cbfff0a6cedc02aa
dimmmslots <vctrline2048@gmail.com>
2023-07-22 15:41:58 +0700

    kurang persist data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
+import { useState } from 'react';
 import CountBtn from '@/components/CountBtn';
-import ReactSVG from '@/assets/react.svg';
-import { Badge } from '@/components/ui/badge';
+import Image100 from './assets/100.png';
+import Image200 from './assets/200.png';
+import Image500 from './assets/500.png';
+import Image1000 from './assets/1000.png';
+
 
 function App() {
+  const receh = [100, 200, 500, 1000];
+  const [receh100, setReceh100] = useState(0);
+  const [receh200, setReceh200] = useState(0);
+  const [receh500, setReceh500] = useState(0);
+  const [receh1000, setReceh1000] = useState(0);
+
+
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <div className="flex flex-col items-center gap-y-4">
-        <div className="inline-flex items-center gap-x-4">
-          <img src={ReactSVG} alt="React Logo" className="w-32" />
-          <span className="text-6xl">+</span>
-          <img src={'/vite.svg'} alt="Vite Logo" className="w-32" />
+        <h1 className='text-6xl mb-8'>Rp. {(receh100 * 100)
+         + (receh200 * 200) + (receh500 * 500) + (receh1000 * 1000)
+         }
+         </h1>
+        <div className="flex flex-row">
+          <CountBtn count={receh100} setCount={setReceh100} text='100' className='mx-1 w-24' imagePath={Image100}/>
+          <CountBtn count={receh200} setCount={setReceh200} text='100' className='mx-1 w-24' imagePath={Image200}/>
+          <CountBtn count={receh500} setCount={setReceh500} text='100' className='mx-1 w-24' imagePath={Image500}/>
+          <CountBtn count={receh1000} setCount={setReceh1000} text='100' className='mx-4 w-24' imagePath={Image1000}/>
         </div>
-        <a href="https://ui.shadcn.com" rel="noopener noreferrer nofollow" target="_blank">
-          <Badge variant="outline">shadcn/ui</Badge>
-        </a>
-        <CountBtn />
       </div>
     </main>
   );
--- src/components/CountBtn.tsx
+++ src/components/CountBtn.tsx
@@ -1,17 +1,25 @@
-import { useState } from 'react';
+// CountBtn.js
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 
 interface Props {
   className?: string;
+  count: number;
+  setCount: (count: number) => void;
+  text: string;
+  imagePath: string; 
 }
 
-export default function CountBtn({ className }: Props) {
-  const [count, setCount] = useState(0);
-
+export default function CountBtn({ className, count, setCount, text, imagePath }: Props) {
   return (
-    <Button onClick={() => setCount((count) => count + 1)} className={cn('', className)}>
-      Count is: {count}
+    <div className='flex flex-col items-center'>
+    <img src={imagePath} alt={text} className='w-fit h-fit cursor-pointer' onClick={() => setCount(count + 1)} />
+    <div className='flex flex-col'>
+    <p className='text-center text-2xl'>{count}x</p>
+    <Button variant={'outline'} onClick={() => {count !== 0 ? setCount(count - 1) : 0}} className={cn('bg-orange-400 text-white', className)}>
+      - 
     </Button>
+    </div>
+    </div>
   );
 }
